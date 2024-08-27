eea657b5790ebebd9cdc2a80ffb7a8b80d9f56ea
Taylor Beck <beck.taylorg@gmail.com>
2024-08-27 05:09:38 -0700

    Add shadcn util

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 /**
  * Formats a number as currency with commas and two decimal places.
  * @param value - The number to format
